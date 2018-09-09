7cac8c05681deed24bfc56804aa23ce01f11d0b0
TheoVitkovskiy <fvitkovski@mail.de>
2018-09-09 18:37:00 +0200

    add processing of tokenized answer
    it's some fairly simple logic
    tries to mimic the behaviour of search engines like solr/es
    
    should also think about moving this sort of logic to some kind of service

--- src/controllers/card.js
+++ src/controllers/card.js
@@ -12,7 +12,17 @@ module.exports.get = async (req, res) => {
 };
 
 module.exports.post = async (req, res) => {
-    const card = await Card.create(req.body);
+    const { question, answer, tokenizedAnswerJson } = req.body;
+
+    const processedTokenizedAnswerJson = JSON.stringify(processTokenizedAnswer(tokenizedAnswerJson));
+
+    const data = {
+        question,
+        answer,
+        tokenizedAnswerJson: processedTokenizedAnswerJson
+    }
+
+    const card = await Card.create(data);
 
     res.json(await Card.findOne({ _id: card._id }));
 };
@@ -20,3 +30,69 @@ module.exports.post = async (req, res) => {
 module.exports.delete = async (req, res) => {
     res.send('Here the card with the id of ' + req.params.cardId + ' will be deleted.');
 };
+
+// ----------------------------
+// processTokenizedAnswer START
+// ----------------------------
+
+function processTokenizedAnswer (tokenizedAnswerJson) {
+    const parsedTokenizedAnswer = JSON.parse(tokenizedAnswerJson);
+    const tokens = Object.keys(parsedTokenizedAnswer);
+
+    const processedTokenizedAnswer = tokens.reduce((processedAnswer,token) => {
+        const weight = parsedTokenizedAnswer[token];
+        const preparedToken = prepareToken(token);
+        const similarTokens = generateSimilarTokens(preparedToken, weight);
+
+        processedAnswer[preparedToken] = {
+            weight,
+            similarTokens
+        };
+
+        return processedAnswer;
+    }, {});
+
+    return processedTokenizedAnswer;
+}
+
+function prepareToken (token) {
+    const allPunctuation = /[~`!@#$%^&*(){}\[\];:"'<,.>?\/\\|_+=-]/g;
+    return token.trim().toLowerCase().replace(allPunctuation, "");
+}
+
+function generateSimilarTokens (token, weight) {
+    const similarTokens = [];
+    
+    if (weight === 0) {
+        return similarTokens;
+    }
+
+    const charsToOmit = calculateCharsToOmit(token.length);
+
+    for (let i = 1; i < charsToOmit; i++) {
+        const slicedToken = token.slice(0, -i);
+        similarTokens.push(slicedToken);
+    }
+
+    return similarTokens;
+}
+
+function calculateCharsToOmit (tokenLength) {
+    if (tokenLength < 3) {
+        return 0;
+    } else if (tokenLength < 6) {
+        return 2;
+    } else if (tokenLength < 9) {
+        return 3;
+    } else if (tokenLength < 12) {
+        return 5;
+    } else if (tokenLength < 15) {
+        return 6;
+    } else {
+        return 7;
+    }
+}
+
+// ----------------------------
+// processTokenizedAnswer END
+// ----------------------------
--- src/models/card.js
+++ src/models/card.js
@@ -11,7 +11,7 @@ module.exports = CardSchema = new mongoose.Schema(
             type: String,
             required: true
         },
-        tokenizedAnswer: {
+        tokenizedAnswerJson: {
             type: String,
             required: true
         },
