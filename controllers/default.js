07b4fe9fa6d8432e2780bbdb368b0ed257ce2eb8
TheoVitkovskiy <fvitkovski@mail.de>
2018-09-09 12:11:34 +0200

    delete and move some files

--- controllers/default.js
+++
@@ -1,7 +0,0 @@
-module.exports.get = (req, res) => {
-    res.status(200).send('pong');
-}
-
-module.exports.post = (req, res) => {
-    res.status(200).send('pong');
-}
--- routes/card.js
+++
@@ -1,9 +0,0 @@
-const Card = require('../controllers/card');
-
-module.exports = api => {
-    api.route('/cards').get(Card.list);
-    api.route('/cards/:cardId').get(Card.get);
-
-    api.route('/cards').post(Card.post);
-    api.route('/cards/:cardId').delete(Card.delete);
-};
